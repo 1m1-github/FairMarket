52f038b06bafce78d581d06eaa0b0e00b2b0d65d
1m1 <email@1m1.io>
2023-04-13 09:17:38 -0600

    no withdraw, no reputation

--- manage.sh
+++ manage.sh
@@ -54,7 +54,6 @@ goal app update --from=$SELLER --app-id=$SELLER_FAIRMARKET_APP --approval-prog $
 
 # update_params [seller]
 
-
 # create bid/trade [bidder]
 export AMOUNT=1
 export ASSET_ID=10458941
@@ -80,24 +79,6 @@ tealdbg debug $TEAL_DIR/$APPROVAL_FILE_NAME.teal -d $TXNS_DIR/dryrun.json --grou
 
 # cancel bid [bidder]
 goal app call --from $BIDDER --app-id $SELLER_FAIRMARKET_APP --app-arg "str:cancel_bid" --app-arg $BID_ID --box $BID_ID --foreign-asset $ASSET_ID --fee 2000
-
-# add reputation [bidder]
-export REPUTATION=1
-export REPUTATION=-1
-goal app call --from $BIDDER --app-id $SELLER_FAIRMARKET_APP --app-arg "str:add_reputation" --app-arg $REPUTATION --out $TXNS_DIR/add_reputation.txn
-goal clerk dryrun -t $TXNS_DIR/add_reputation.stxn --dryrun-dump -o $TXNS_DIR/dryrun.json
-tealdbg debug $TEAL_DIR/$APPROVAL_FILE_NAME.teal -d $TXNS_DIR/dryrun.json --group-index 0 --mode application
-goal clerk rawsend --filename $TXNS_DIR/add_reputation.stxn
-
-# withdraw [seller]
-goal asset send --from $SELLER_FAIRMARKET_ACCOUNT --to $SELLER --amount $AMOUNT --assetid $ASSET_ID --out $TXNS_DIR/withdraw_send.txn
-goal app call --from $SELLER --app-id $SELLER_FAIRMARKET_APP --app-arg "str:withdraw" --out $TXNS_DIR/withdraw_call.txn
-cat $TXNS_DIR/withdraw_send.txn $TXNS_DIR/withdraw_call.txn > $TXNS_DIR/combined.txn
-goal clerk group --infile $TXNS_DIR/combined.txn --outfile $TXNS_DIR/withdraw.txn
-goal clerk sign --infile $TXNS_DIR/withdraw.txn --outfile $TXNS_DIR/withdraw.stxn
-goal clerk dryrun -t $TXNS_DIR/withdraw.stxn --dryrun-dump -o $TXNS_DIR/dryrun.json
-tealdbg debug $TEAL_DIR/$APPROVAL_FILE_NAME.teal -d $TXNS_DIR/dryrun.json --group-index 1 --mode application
-goal clerk rawsend --filename $TXNS_DIR/withdraw.stxn
 
 #debug
 goal app read --app-id $SELLER_FAIRMARKET_APP --global

73c35da8e06e8d0db90b1c8bc4df2e0ce6ab8b95
1m1 <email@1m1.io>
2023-06-15 12:44:05 -0600

    send notification to B on create_bid

--- manage/manage_code.sh
+++ manage/manage_code.sh
@@ -26,7 +26,7 @@ goal app update --from=$CREATOR --app-id=$FAIRMARKET_APP --approval-prog $TEAL_D
 # create bid [A]
 goal app call --from $A --app-id $FX_APP --foreign-app $FX_LP_APP --foreign-asset $CURRENCY_ID --app-account $FX_LP_ACCOUNT --out $TXNS_DIR/FX.txn --fee 0
 goal clerk send --from $A --to $FAIRMARKET_ACCOUNT --amount 283300 --out $TXNS_DIR/algo_send.txn --fee 0
-goal app call --from $A --app-id $FAIRMARKET_APP --foreign-asset $CURRENCY_ID --app-arg "str:create_bid" --app-arg "addr:$B" --app-arg "b64:$BID_ID_B64" --box "addr:$B" --box "b64:$BID_ID_B64" --out $TXNS_DIR/app_call.txn --fee 5000
+goal app call --from $A --app-id $FAIRMARKET_APP --foreign-asset $CURRENCY_ID --app-arg "str:create_bid" --app-arg "addr:$B" --app-arg "b64:$BID_ID_B64" --box "addr:$B" --box "b64:$BID_ID_B64" --app-account $B --out $TXNS_DIR/app_call.txn --fee 6000
 goal asset send --from $A --to $FAIRMARKET_ACCOUNT --amount $CURRENCY_AMOUNT --assetid $CURRENCY_ID --noteb64 $CREATE_NOTE_B64 --out $TXNS_DIR/asset_send.txn --fee 0
 cat $TXNS_DIR/FX.txn $TXNS_DIR/algo_send.txn $TXNS_DIR/app_call.txn $TXNS_DIR/asset_send.txn > $TXNS_DIR/combined.txn
 goal clerk group --infile $TXNS_DIR/combined.txn --outfile $TXNS_DIR/create_bid.txn
@@ -34,7 +34,7 @@ goal clerk sign --infile $TXNS_DIR/create_bid.txn --outfile $TXNS_DIR/create_bid
 goal clerk rawsend --filename $TXNS_DIR/create_bid.stxn
 
 # cancel bid [bidder]
-goal app call --from $A --app-id $FAIRMARKET_APP --app-arg "str:cancel_bid" --app-arg "b64:$BID_ID" --box "b64:$BID_ID" --foreign-asset $CURRENCY_ID --fee 3000
+goal app call --from $A --app-id $FAIRMARKET_APP --app-arg "str:cancel_bid" --app-arg "b64:$BID_ID_B64" --box "b64:$BID_ID_B64" --foreign-asset $CURRENCY_ID --fee 3000
 
 # trade [seller]
 goal asset optin --account $B --assetid $CURRENCY_ID --fee 0 --out $TXNS_DIR/trade_optin.txn
